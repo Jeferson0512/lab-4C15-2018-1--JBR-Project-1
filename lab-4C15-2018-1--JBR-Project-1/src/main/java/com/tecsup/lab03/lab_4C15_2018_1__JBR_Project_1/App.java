1ea7685d4d5fb34ff1e1d6bb62da3ce5b2f76a0d
Alumno <Alumno@LAB701-18>
2018-02-26 10:08:56 -0500

    first commit

---
+++ lab-4C15-2018-1--JBR-Project-1/src/main/java/com/tecsup/lab03/lab_4C15_2018_1__JBR_Project_1/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab_4C15_2018_1__JBR_Project_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
