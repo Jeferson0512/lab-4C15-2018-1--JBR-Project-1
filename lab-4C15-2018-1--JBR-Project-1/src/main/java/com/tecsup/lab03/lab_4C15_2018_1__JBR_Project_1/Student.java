e03d63cacac5c7239b97a47c65896c0dc4fffd35
Alumno <Alumno@LAB701-21>
2018-02-26 11:17:06 -0500

    creaciòn de clase student...

---
+++ lab-4C15-2018-1--JBR-Project-1/src/main/java/com/tecsup/lab03/lab_4C15_2018_1__JBR_Project_1/Student.java
@@ -0,0 +1,11 @@
+package com.tecsup.lab03.lab_4C15_2018_1__JBR_Project_1;
+
+public class Student {
+
+	public static void main( String[] args )
+    {
+		System.out.println("hola BUJAICO");	
+    }
+	
+	
+}
